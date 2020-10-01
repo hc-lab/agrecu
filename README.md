5b4284ff962e4d5dd3556d533a60abafed26a211
Hitkeler <kenhitters@gmail.com>
2020-10-01 12:57:34 -0500

    e

--- README.md
+++
@@ -1,2 +0,0 @@
-# agrecu
-Tienda Virtual AGRECU
--- agrecu/productos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Producto, Comentario, ImagenesProducto, CarritoCompra
-
-@admin.register(Producto, Comentario, ImagenesProducto, CarritoCompra)
-class AuthorAdmin(admin.ModelAdmin):
-    pass
