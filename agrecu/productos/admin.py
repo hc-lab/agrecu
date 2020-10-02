65fc8e2d3884af05433df7d4884af44c0d059126
Hitkeler <kenhitters@gmail.com>
2020-10-02 03:07:35 -0500

    ww

--- agrecu/productos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Producto, Comentario, ImagenesProducto, CarritoCompras
-
-@admin.register(Producto,Comentario,ImagenesProducto, CarritoCompras)
-class AuthorAdmin(admin.ModelAdmin):
-    pass
